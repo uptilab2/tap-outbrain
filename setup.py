5798a25995e2ce4bc77d70ff38408008afcd5d6c
Dan Mosora <dan@stitchdata.com>
2018-11-12 16:50:53 +0000

    Version 0.3.2, bump requests to 2.20.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='tap-outbrain',
-      version='0.3.1',
+      version='0.3.2',
       description='Singer.io tap for extracting data from the Outbrain API',
       author='Fishtown Analytics',
       url='http://singer.io',
@@ -13,7 +13,7 @@ setup(name='tap-outbrain',
       install_requires=[
           'singer-python==5.0.4',
           'backoff==1.3.2',
-          'requests==2.12.4',
+          'requests==2.20.0',
           'python-dateutil==2.6.0'
       ],
       entry_points='''
